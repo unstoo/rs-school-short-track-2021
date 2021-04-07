9018a75147778a4541396ed25620938acd9c0e60
unstoo <unstoo@gmail.com>
2021-04-07 15:09:18 +0300

    feat: solve delete digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -7,10 +7,30 @@
  *
  * @example
  * For n = 152, the output should be 52
- *
+ * 132156
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const digits = String(n)
+    .split('')
+    .map(Number);
+
+  let prev = digits[0];
+  let index = 0;
+  let stopFlag = false;
+
+  for (let i = 0; i < digits.length; i += 1) {
+    if (digits[i] < prev && stopFlag === false) {
+      prev = digits[i];
+      index = i;
+    }
+
+    if (digits[i] > prev && stopFlag === false) {
+      stopFlag = true;
+    }
+  }
+
+  digits.splice(index, 1);
+  return Number(digits.join(''));
 }
 
 module.exports = deleteDigit;
