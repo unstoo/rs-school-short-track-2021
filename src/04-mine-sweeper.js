d289cecefb973da53eed4d8642a0de3ac08fd64c
unstoo <unstoo@gmail.com>
2021-04-07 14:50:29 +0300

    feat: implement mines sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,79 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function countAround(m, row, col) {
+  let minesCount = 0;
+  const rows = m.length - 1;
+  const cols = m[0].length - 1;
+  // mine
+  if (m[row][col] === true) {
+    return 1;
+  }
+
+  // west
+  if (col > 0) {
+    if (m[row][col - 1]) {
+      minesCount += 1;
+    }
+  }
+  // east
+  if (col < cols) {
+    if (m[row][col + 1]) {
+      minesCount += 1;
+    }
+  }
+  // north
+  if (row > 0) {
+    if (m[row - 1][col]) {
+      minesCount += 1;
+    }
+  }
+  // south
+  if (row < rows) {
+    if (m[row + 1][col]) {
+      minesCount += 1;
+    }
+  }
+  // north-west
+  if (col > 0 && row > 0) {
+    if (m[row - 1][col - 1]) {
+      minesCount += 1;
+    }
+  }
+  // south-west
+  if (col > 0 && row < rows) {
+    if (m[row + 1][col - 1]) {
+      minesCount += 1;
+    }
+  }
+  // south-east
+  if (col < cols && row < rows) {
+    if (m[row + 1][col + 1]) {
+      minesCount += 1;
+    }
+  }
+  // north-east
+  if (col < cols && row > 0) {
+    if (m[row - 1][col + 1]) {
+      minesCount += 1;
+    }
+  }
+
+  return minesCount;
+}
+
+function minesweeper(m) {
+  const rows = m.length;
+  const cols = m[0].length;
+  const result = [];
+  for (let rowIndex = 0; rowIndex < rows; rowIndex += 1) {
+    result.push([]);
+    for (let colIndex = 0; colIndex < cols; colIndex++) {
+      const mines = countAround(m, rowIndex, colIndex);
+      result[rowIndex].push(mines);
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
