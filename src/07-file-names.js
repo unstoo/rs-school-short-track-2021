404f7d92ae77c0448ade416f4fda2ede2345f115
unstoo <unstoo@gmail.com>
2021-04-09 13:26:45 +0300

    feat: solve file names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,25 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const dict = {};
+  const result = [];
+
+  for (let i = 0; i < names.length; i += 1) {
+    const name = names[i];
+
+    if (dict[name] === undefined) {
+      dict[name] = 1;
+      result.push(name);
+    } else {
+      const suffixed = `${name}(${dict[name]})`;
+      dict[name] += 1;
+      dict[suffixed] = 1;
+      result.push(suffixed);
+    }
+  }
+
+  return result;
 }
 
 module.exports = renameFiles;
